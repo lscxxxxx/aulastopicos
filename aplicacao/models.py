80c52cac0e48bee84133009a95d1e4b4c5055b41
lscxxxxx <lucas.batistanunes7@gmail.com>
2025-08-18 17:16:16 -0300

    banco da aula 01

--- aplicacao/models.py
+++ aplicacao/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    nome = models.CharField("Nome", max_length = 200, null = True)
+    preco = models.DecimalField("Preço", decimal_places = 2, max_digits = 8)
+    qtd = models.PositiveIntegerField("Quantidade", default = 0, null = True)
+    def __str__(self):
+        return self.nome
